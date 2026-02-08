b43e49fad89cf586947d58af7e6e8f2a17674168
pablofsc <pablofsc@outlook.com>
2026-02-08 13:33:13 -0300

    improve SelectedVehicleNotifier

--- lib/provider/selected_vehicle_provider.dart
+++ lib/provider/selected_vehicle_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart' as riverpod;
 
 import 'package:carman/models/vehicle.dart';
 import 'package:carman/repositories/vehicle_repository.dart';
+import 'package:carman/provider/vehicles_provider.dart';
 
 final selectedVehicleProvider =
     riverpod.AsyncNotifierProvider<SelectedVehicleNotifier, Vehicle?>(
@@ -11,15 +12,25 @@ final selectedVehicleProvider =
 class SelectedVehicleNotifier extends riverpod.AsyncNotifier<Vehicle?> {
   @override
   Future<Vehicle?> build() async {
-    return await VehicleRepository.getSelected();
+    final selected = (await VehicleRepository.getSelected())?.id;
+
+    if (selected == null) return null;
+
+    return ref
+        .watch(vehiclesProvider)
+        .maybeWhen(
+          data: (vehicles) => vehicles.firstWhere((v) => v.id == selected),
+          orElse: () => null,
+        );
   }
 
   Future<void> set(String id) async {
     // TODO: improve this
-    final vehicle = await VehicleRepository.getVehicleById(id);
+    final vehicles = ref.read(vehiclesProvider).requireValue;
+    final selected = vehicles.firstWhere((v) => v.id == id);
 
-    state = riverpod.AsyncData(vehicle);
+    state = riverpod.AsyncData(selected);
 
-    await VehicleRepository.setSelected(id);
+    VehicleRepository.setSelected(id);
   }
 }
